3b9e3f808c9a30cde9d23910651ca8480ff0f918
sergystepanov <sergystepanov@gmail.com>
2024-01-06 19:16:11 +0300

    Migrate Table page to TypeScript

---
+++ src/renderer/components/date/_date.ts
@@ -0,0 +1,48 @@
+import moment from 'moment';
+
+/**
+ * The class for date manipulations.
+ *
+ * @since 0.1.0
+ */
+export default class _date {
+  // Returns the amount of minutes passed since 00:00 of the date.
+  static toMinutes = (date: Date): number =>
+    moment.duration(`${date.getHours()}:${date.getMinutes()}`).asMinutes();
+
+  // Adds given value of minutes to a date.
+  static toDate = (minutes: number): Date =>
+    moment('1945-05-09').add(minutes, 'minutes').toDate();
+
+  // Converts the number of minutes into the hh:mm formatted string.
+  static minutesToHm(minutes: number): string {
+    if (minutes < 1) {
+      return '00:00';
+    }
+
+    const h = Math.trunc(minutes / 60);
+    const m = minutes % 60;
+
+    return `${h < 10 ? `0${h}` : h}:${m < 10 ? `0${m}` : m}`;
+  }
+
+  // Converts a formatted string into date object.
+  static fromYmd = (value: string): Date =>
+    moment(value, 'YYYY-MM-DD').toDate();
+
+  // Converts the date into YYYY-MM-DD format.
+  static toYmd = (date: Date): string => moment(date).format('YYYY-MM-DD');
+
+  // Re-formats a date string with the provided format.
+  static format = (date: string, format: string): string =>
+    moment(date).format(format);
+
+  // Returns the current date.
+  static current = (): string => moment().format('YYYY-MM-DD');
+
+  // Returns two dates in the array as [-3 days, +3 days] counting from the current date.
+  static getThreeDaysRange = (): [Date, Date] => [
+    moment().subtract(3, 'days').toDate(),
+    moment().add(3, 'days').toDate(),
+  ];
+}
---
+++ src/renderer/containers/TableContainer.tsx
@@ -0,0 +1,152 @@
+import { useEffect, useState } from 'react';
+
+import { DateRange } from '@blueprintjs/datetime2';
+
+import { Api } from 'cosmoport-core-api-client';
+import Table from '../components/table/Table';
+import Message from '../components/messages/Message';
+import _date from '../components/date/_date';
+
+import {
+  EventType,
+  EventFormDataType,
+  GateType,
+  LocaleType,
+  RefsType,
+} from '../types/Types';
+
+type Props = {
+  api: Api;
+  auth?: boolean;
+  onRefresh?: () => void;
+  pre: number;
+};
+
+type State = {
+  events: EventType[];
+  gates: GateType[];
+  locale: LocaleType;
+  defaultRange: DateRange;
+  range: DateRange;
+  refs: RefsType;
+};
+
+export default function TableContainer({
+  api,
+  auth = false,
+  onRefresh = () => {},
+  pre,
+}: Props) {
+  const [state, setState] = useState<State>({
+    events: [],
+    locale: {},
+    refs: {
+      destinations: [],
+      statuses: [],
+      states: [],
+      types: [],
+      type_categories: [],
+    },
+    gates: [],
+    defaultRange: _date.getThreeDaysRange(),
+    range: _date.getThreeDaysRange(),
+  });
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    Promise.all([
+      api.fetchReferenceData(),
+      api.fetchTranslations(),
+      apiGetEventInRange(state.range),
+      api.fetchGates(),
+    ])
+      .then((data) =>
+        setState({
+          ...state,
+          refs: data[0],
+          locale: data[1].en,
+          events: data[2],
+          gates: data[3],
+        }),
+      )
+      .catch(console.error);
+  };
+
+  const handleCreate = (
+    formData: EventFormDataType,
+    suggester: (time: number) => void,
+  ) => {
+    api
+      .createEvent(formData)
+      .then((result) => Message.show(`Event has been created [${result.id}].`))
+      .then(() => suggester && suggester(pre))
+      .then(() => handleRefresh())
+      .catch(console.error);
+  };
+
+  const handleEdit = (formData: EventFormDataType) => {
+    api
+      .updateEvent(formData)
+      .then((result) => {
+        Message.show(`Event has been updated [${result.id}].`);
+        handleRefresh();
+      })
+      .catch(console.error);
+  };
+
+  const handleDelete = (id: number) => {
+    api
+      .deleteEvent(id)
+      .then((result) => {
+        Message.show(`Deleted ${result.deleted}.`);
+        handleRefresh();
+      })
+      .catch(console.error);
+  };
+
+  const handleRefresh = () => {
+    getData();
+    onRefresh();
+  };
+
+  const handleDateChange = (range_: DateRange) => {
+    apiGetEventInRange(range_)
+      .then((data) => setState({ ...state, events: data, range: range_ }))
+      .catch(console.error);
+  };
+
+  const handleDateClear = () => {
+    handleDateChange(state.defaultRange);
+  };
+
+  const apiGetEventInRange = (range: DateRange) =>
+    api.get(
+      `/timetable?date=${range[0] ? _date.toYmd(range[0]) : ''}&date2=${
+        range[1] ? _date.toYmd(range[1]) : ''
+      }`,
+    );
+
+  const { events, gates, locale, range, refs } = state;
+
+  return events.length > 0 ? (
+    <Table
+      auth={auth}
+      events={events}
+      gates={gates}
+      locale={locale}
+      onCreate={handleCreate}
+      onDateRangeChange={handleDateChange}
+      onDateRangeClear={handleDateClear}
+      onDelete={handleDelete}
+      onEdit={handleEdit}
+      onRefresh={handleRefresh}
+      range={range}
+      refs={refs}
+    />
+  ) : (
+    'No data...'
+  );
+}
