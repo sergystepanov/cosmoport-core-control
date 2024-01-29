5c732c89fcfd09e4a496522622e507ceebf7f07e
sergystepanov <sergystepanov@gmail.com>
2024-01-29 22:46:50 +0300

    Remove old React props

--- src/renderer/props/GatePropType.js
+++
@@ -1,7 +0,0 @@
-import PropTypes from 'prop-types';
-
-export default PropTypes.shape({
-  id: PropTypes.number,
-  number: PropTypes.number,
-  gateName: PropTypes.string
-});
--- src/renderer/props/LocalePropType.js
+++
@@ -1,6 +0,0 @@
-import PropTypes from 'prop-types';
-
-export default PropTypes.objectOf(PropTypes.shape({
-  id: PropTypes.number,
-  values: PropTypes.arrayOf(PropTypes.string)
-}));
